8465fb16396642c5f67b0b9ab6b30fcf44fafa59
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-06-16 15:34:44 -0300

    Primer commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { User, UserSchema } from './entities/user.entity';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [ 
+    ConfigModule.forRoot(),       // esta config es para que reconozca las variables de entornos
+    MongooseModule.forFeature([ 
+      {
+        name: User.name,
+        schema: UserSchema,
+      }
+    ]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SEED,
+      signOptions: { expiresIn: '6h' },
+    }),
+  ]
+})
+export class AuthModule {}
